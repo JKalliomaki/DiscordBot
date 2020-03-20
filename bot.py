cbeffa5f83a305fa3f9d32ae0f14e50db76b0ed9
Jussi Kalliomäki <jussi.kalliomaki99@gmail.com>
2020-03-20 16:24:31 +0200

    Play should work

--- bot.py
+++ bot.py
@@ -1,10 +1,13 @@
 import discord
 from discord.ext import commands
+import youtube_dl
 
 client = commands.Bot(command_prefix='.')
 guild = client.get_guild(263639196403630083)
 default_text_channel = client.get_channel(263639196403630083)
-command_list = ['apua', 'play']
+command_list = ['apua', 'play', 'connect', 'leave']
+
+players = {}
 
 @client.event
 async def on_ready():
@@ -15,11 +18,33 @@ async def commands(ctx):
     response = ", ".join(command_list)
     await ctx.send(f'Commands: {response}')
 
-
 @client.command()
 async def apua(ctx):
     response = "Lol apua :D"
     await ctx.send(response)
+
+@client.command()
+async def connect(ctx):
+    channel = ctx.author.voice.channel
+    await channel.connect()
+
+@client.command()
+async def leave(ctx):
+    await ctx.voice_client.disconnect()
+
+@client.command()
+async def play(ctx, url):
+    try:
+        server = ctx.message.server
+        voice_client = client.voice_client_in(server)
+
+        player = await voice_client.create_ytdl_player(url)
+        players[server.id] = player
+        player.start()
+
+    except AttributeError:
+        #default_text_channel.send('Uno problemo i biisi')
+        return
 
 
 @client.event
